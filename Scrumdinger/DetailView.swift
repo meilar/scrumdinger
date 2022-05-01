6f8398d9286e064314c10f5546b6e50563d05c29
Matt Eilar <meilar@gmail.com>
2022-05-01 16:56:56 -0700

    Complete details view navigation hierarchy

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -12,9 +12,12 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -30,7 +33,13 @@ struct DetailView: View {
                         .cornerRadius(4)
                 }
             }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
